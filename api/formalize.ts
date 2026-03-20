4ecc235ca79001ce44c8c0cd6c0ea64931199886
0art0 <18333981+0art0@users.noreply.github.com>
2026-03-20 11:44:29 +0000

    Reorganize endpoints

--- api/formalize.ts
+++ api/formalize.ts
@@ -1,1 +1,19 @@
-export { default } from '../src/index'
+import { formalizeStatementServer } from '../src/endpoints/Formalize'
+
+export default async function handler(req: any, res: any) {
+  console.log("formalizing...", req.body.problem)
+  const problem = req.body.problem
+  if (!problem) {
+    console.error("no problem provided")
+    res.send("FAILED: no problem provided")
+    return
+  }
+  try {
+    const result = await formalizeStatementServer(problem)
+    console.log("formalized", result)
+    res.json(result)
+  } catch (err) {
+    console.error(err)
+    res.send("FAILED: " + err)
+  }
+}
