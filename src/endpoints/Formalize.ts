e82e76defd905c193b7d1e8e24c7bac6e1861c1c
0art0 <18333981+0art0@users.noreply.github.com>
2026-03-21 23:58:59 +0000

    Remove stray comma in JSON structure

--- src/endpoints/Formalize.ts
+++ src/endpoints/Formalize.ts
@@ -11,7 +11,7 @@ export async function formalizeStatement({ problem }: { problem: string }): Prom
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      problem,
+      problem
     }),
   })
 
