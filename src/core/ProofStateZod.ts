b8a53d4251e19969307d6b6c234ca5cffd5354e1
0art0 <18333981+0art0@users.noreply.github.com>
2026-03-30 01:27:18 +0100

    Spacing in multiplication Typst

--- src/core/ProofStateZod.ts
+++ src/core/ProofStateZod.ts
@@ -13,6 +13,7 @@ export const AtomicStatementSchema = z.string().describe(`A basic statement with
 Some examples are:
 - "$f$ is injective"
 - "$G$ is abelian"
+- "$x y x^(-1)$ has order 2" 
 - "$x^2 + 1$ is an irreducible polynomial over $RR$
 
 When expressions contain infix operators, it becomes difficult for Typst to recognize sequences of symbols that form a single mathematical expression. In such cases, it is recommended to use parentheses to group the expression together, for example, $x = (a + b + c)$ instead of $x = a + b + c$.
