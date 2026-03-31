7d0c22a6988c75fb0abda207b822b24138af802e
0art0 <18333981+0art0@users.noreply.github.com>
2026-03-31 08:41:05 +0100

    Switch back to OpenAI

--- src/endpoints/models.ts
+++ src/endpoints/models.ts
@@ -4,5 +4,5 @@ export const MODELS = {
     "move": "openai/gpt-5.4",
     "filter": "openai/gpt-5.3-chat",
     "informalize": "inception/mercury-2",
-    "suggest": "anthropic/claude-sonnet-4.6"
+    "suggest": "openai/gpt-5.4"
 }
